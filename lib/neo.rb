666bdcaed05916fc2476bc8db43cc848fed476dd
andreasr <andreasr@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2008-05-20 16:38:35 +0000

    refactoring, sorry did overwrite the neo.rb - svn conflict
    git-svn-id: https://svn.neo4j.org/laboratory/neo4j.rb@1559 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- lib/neo.rb
+++ lib/neo.rb
@@ -2,7 +2,8 @@ include Java
 
 
 module Neo
-  
+  require 'singleton'
+
   require 'neo-1.0-b6.jar'
   require 'jta-spec1_0_1.jar'
   require 'index-util-0.4-20080512.110337-6.jar'
@@ -16,49 +17,301 @@ module Neo
   ReturnableEvaluator = org.neo4j.api.core.ReturnableEvaluator
   Direction = org.neo4j.api.core.Direction
   IndexService = org.neo4j.util.index.IndexService
+  
+  #
+  # Allows start and stop of Neo
+  # 
+  # A wrapper class around org.neo4j.api.core.EmbeddedNeo
+  # 
+  class NeoService
+    include Singleton
 
-  # Need to put these here because of missing constant errors otherwise
-  class Node
-  end
-  
-  class MetaNode < Node
-  end
-  
-  class MetaNodes < Node
-  end
-
-  
-  
-  def self.start
-    puts "start neo"
-    # @@neo = EmbeddedNeo.new("var/neo")  
-    Neo::Connection.establish_connection
+    #
+    # meta_nodes : Return the meta nodes containing relationship to all MetaNode objects
+    #
+    attr_reader :meta_nodes 
     
-    # add a super node having subnodes to all classnamees
-    # a classname is a node that is created for each time someone inherits from the Node class
-    transaction do
-      # TODO there should only be one metanodes object (@@neo.getReferenceNode)
-      @@meta_nodes = MetaNodes.new #(@@neo.getReferenceNode)
+    #
+    # starts neo with a database at the given storage location
+    # 
+    def start(storage = "var/neo")
+      raise Exception.new("Already started neo") if @neo
+      puts "start neo"
+      @neo = EmbeddedNeo.new(storage)  
+      
+      ref_node = nil
+      Neo::transaction do
+        ref_node = @neo.getReferenceNode
+        @meta_nodes = MetaNodes.new(ref_node)
+      end
+    end
+    
+    
+    #
+    # Create an internal neo node (returns a java object)
+    #
+    def create_node
+      @neo.createNode
+    end
+    
+    # 
+    # Find the meta node represented by the given Ruby class name
+    #
+    def find_meta_node(classname) 
+      @meta_nodes.nodes.find{|node| node.meta_classname == classname}    
+    end
+    
+    #
+    # Returns a Node object that has the given id or nil
+    # 
+    def find_node(id) 
+      neo_node = @neo.findNodeById(id)
+      load_node(neo_node)
+    end
+  
+    def load_node(neo_node)
+      classname = neo_node.get_property('classname')
+      #      puts "Load node #{neo_node} class #{classname}"
+      # get the class that might exist in a module
+      clazz = classname.split("::").inject(Kernel) do |container, name|
+        container.const_get(name.to_s)
+      end
+      clazz.new(neo_node)
+    end
+    
+    #
+    # Stop neo
+    # Must be done before the program stops
+    #
+    def stop
+      puts "stop neo #{@neo}"
+      @neo.shutdown  
+      @neo = nil
     end
     
   end
+  
+  
+  #
+  # A wrapper around a Java neo node
+  # 
+  #
+  class Node
+    attr_reader :internal_node 
+    
+    #
+    # Must be run in an transaction unless a block is given 
+    # If a block is given a new transaction will be created
+    #    
+    def initialize(*args)
+      if args.length == 1 and args[0].kind_of?(org.neo4j.api.core.Node)
+        @internal_node = args[0]
+        self.classname = self.class.to_s unless @internal_node.hasProperty("classname")
+      elsif block_given? # check if we should run in a transaction
+        Neo.transaction { init_internal; yield self }
+      else
+        init_internal
+      end
+    end
+    
+    def init_internal
+      @internal_node = Neo::neo_service.create_node
+      self.classname = self.class.to_s
+      # TODO set classname node to point to self
+    end
 
-  def self.meta_nodes
-    @@meta_nodes
+    def method_missing(methodname, *args)
+      # allows to set and get any neo property without declaring them first
+      name = methodname.to_s
+      setter = /=$/ === name
+      expected_args = 0
+      if setter
+        name = name[0...-1]
+        expected_args = 1
+      end
+      unless args.size == expected_args
+        err = "method '#{name}' has wrong number of arguments (#{args.size} for #{expected_args})"
+        raise ArgumentError.new(err)
+      end
+
+      raise Exception.new("Node not initialized, called method '#{methodname}' on #{self.class.to_s}") unless @internal_node
+      
+      if setter
+        @internal_node.set_property(name, args[0])
+      else
+        if !@internal_node.has_property(name)
+          super.method_missing(methodname, *args)
+        else        
+          @internal_node.get_property(name)
+        end      
+      end
+    end
+    
+    
+    # 
+    # Returns a unique id
+    # Calls getId on the neo node java object
+    #
+    def neo_node_id
+      @internal_node.getId()
+    end
+    
+    
+    def to_s
+      iter = @internal_node.getPropertyKeys.iterator
+      s = self.class.to_s + ", properties:\n"
+      while (iter.hasNext) do
+        p = iter.next
+        s << "'#{p}' = '#{@internal_node.getProperty(p)}'\n"
+      end
+      s
+    end
+    
+    # --------------------------------------------------------------------------
+    # Node class methods
+    #
+    
+    
+    def self.inherited(c)
+      # This method adds a MetaNode for each class that inherits from the Node
+      # must avoid endless recursion 
+      if c == MetaNode or c == MetaNodes
+        return
+      end
+      
+      # create a meta node representing the new class
+      # TODO check: should only be created once  ?      
+      metanode = MetaNode.new do |n|
+        n.meta_classname = c.to_s
+        Neo::neo_service.meta_nodes.nodes << n
+      end
+      
+      # define the 'meta_node' method in the new class
+      classname = class << c;  self;  end
+      classname.send :define_method, :meta_node do
+        metanode
+      end      
+
+    end
+
+    
+    #
+    # Allows to declare Neo properties.
+    # Notice that you do not need to declare any properties in order to 
+    # set and get a neo property.
+    # An undeclared setter/getter will handled in the method_missing method instead.
+    #
+    def self.properties(*props)
+      props.each do |prop|
+        define_method(prop) do 
+          @internal_node.get_property(prop.to_s)
+        end
+
+        name = (prop.to_s() +"=")
+        define_method(name) do |value|
+          @internal_node.set_property(prop.to_s, value)
+        end
+      end
+    end
+    
+    
+    #
+    # Allows to declare Neo relationsships.
+    # The speficied name will be used as the type of the neo relationship.
+    #
+    def self.add_relation_type(type)
+      define_method(type) do 
+        Relations.new(self,type.to_s)
+      end
+    end
+    
+    
+    def self.relations(*relations)
+      relations.each {|type| add_relation_type(type)}
+    end
+
+    properties :classname
+  end
+
+  
+  class Relations
+    include Enumerable
+    
+    def initialize(node, type)
+      @node = node
+      @type = RelationshipType.instance(type)      
+    end
+    
+    def each
+      traverser = @node.internal_node.traverse(org.neo4j.api.core.Traverser::Order::BREADTH_FIRST, 
+        StopEvaluator::DEPTH_ONE,
+        ReturnableEvaluator::ALL_BUT_START_NODE,
+        @type,
+        Direction::OUTGOING)
+      iter = traverser.iterator
+      while (iter.hasNext) do
+        yield Neo::neo_service.load_node(iter.next)
+      end
+    end
+      
+    
+    def <<(other)
+      @node.internal_node.createRelationshipTo(other.internal_node, @type)
+      self
+    end
   end
   
-  def self.find_meta_node(classname) 
-    meta_nodes.nodes.find{|node| node.meta_classname == classname}    
+  
+  class RelationshipType
+    include org.neo4j.api.core.RelationshipType
+    attr_accessor :name 
+
+    @@names = {}
+    
+    def RelationshipType.instance(name)
+      return @@names[name] if @@names.include?(name)
+      @@names[name] = RelationshipType.new(name)
+    end
+
+    def to_s
+      self.class.to_s + " name='#{@name}'"
+    end
+
+    private
+    
+    def initialize(name)
+      @name = name.to_s
+    end
+    
   end
   
-  def self.stop
-    puts "stop neo"
-    Neo::Connection.instance.disconnect
-    # @@neo.shutdown  
-  end
   
-  def self.transaction      
+  class MetaNode < Node
+    properties :meta_classname # the name of the ruby class it represent
+    relations :instances
+  end
+
+  class MetaNodes < Node
+    relations :nodes
+  end
+
+  
+  # ----------------------------------------------------------------------------
+  # Neo Module methods
+  #
+
+  #
+  # Runs a block in a Neo transaction
+  #
+  # All CRUD operations must be run in a transaction
+  # If a block is given then that block will be executed in a transaction, 
+  # otherwise it will simply return a java neo transaction object.
+  #
+  def transaction     
+    return Transaction unless block_given?
+
     tx = Transaction.begin  
+    
     begin  
       yield  
       tx.success  
@@ -69,43 +322,13 @@ module Neo
     end      
   end  
 
-  def self.create_node
-    Neo::Connection.instance.neo.createNode
-    # @@neo.createNode
-  end
-
+  #
+  # Returns a NeoService
+  # 
+  def neo_service
+    NeoService.instance
+  end  
   
-  def self.find_node(id) 
-    # neo_node = @@neo.findNodeById(id)
-    neo_node = Neo::Connection.instance.neo.findNodeById(id)
-    load_node(neo_node)
-  end
-  
-  def self.load_node(neo_node)
-    classname = neo_node.get_property('classname')
-    # get the class that might exist in a module
-    clazz = classname.split("::").inject(Kernel) do |container, name|
-      container.const_get(name.to_s)
-    end
-    clazz.new(neo_node)
-  end
-  
-  
-
-  
-  require 'singleton'
-  require 'connection'
-  require 'node'
-  require 'relationship_type'
-  require 'relations'
-  require 'meta_node'
-  require 'meta_nodes'
-  
-  
-  
-  
-
-  
+  module_function :transaction, :neo_service
 end
-
 
--- test/neo_spec.rb
+++ test/neo_spec.rb
@@ -3,271 +3,302 @@
 # and open the template in the editor.
  
 require 'neo'
+require 'fileutils'  
 
+DB_LOCATION = 'var/neo'
 
-describe Neo do
-  before(:all) do
-    Neo::start
-  end
-
-  after(:all) do
-    Neo::stop
-  end  
-  
-  it "should not find a meta node of a class that does not exist" do
-    n = Neo::find_meta_node('Kalle')
-    n.should be_nil
-  end
-  
-  it "should find the meta node of a class that exists" do
-    class Kalle < Neo::Node 
-    end
-    
-    n = Neo::find_meta_node('Kalle')
-    n.should_not be_nil
-    n.should be_kind_of(Neo::MetaNode)
-  end
- 
-  it "should find an (ruby) object stored in neo given its unique id"
-  #node = Neo::find_node(id) ...
-  
+def start
+  FileUtils.rm_r DB_LOCATION if File.directory?(DB_LOCATION)
+  Neo::NeoService.instance.start(DB_LOCATION)
 end
 
 
+def stop
+  Neo::NeoService.instance.stop
+  FileUtils.rm_r DB_LOCATION if File.directory?(DB_LOCATION)
+end
+
+# ------------------------------------------------------------------------------
+# the following specs are not always run inside ONE Neo transaction
+# 
+
+describe "When using several transactions" do
+
 describe Neo::MetaNode do
   before(:all) do
-    Neo::start
+    start
   end
 
   after(:all) do
-    Neo::stop
+    stop
   end  
-
+    
   it "should find all instances of a class" 
-  
+    
   it "should be unique, when neo is restarted" do
+    # when Neo is restarted make sure that the node representing the class
+    # has the same node_id
+    
     class Foo < Neo::Node
     end
+
+    id1 = nil
+    Neo::transaction {
+      id1 = Neo::neo_service.find_meta_node('Foo').neo_node_id
+    }
+
+    Neo::neo_service.stop
+    Neo::neo_service.start DB_LOCATION
     
-    id1 = Neo::find_meta_node('Foo').neo_node_id
-    Neo::stop
-    Neo::start
-    id2 = Neo::find_meta_node('Foo').neo_node_id # bug, does not even find it again
+    id2 = nil
+    Neo::transaction {
+      id2 = Neo::neo_service.find_meta_node('Foo').neo_node_id
+    }
     id1.should == id2
   end
-  # make sure that the node_id is the same when neo is restarted
 end 
+end
 
+# ------------------------------------------------------------------------------
+# the following specs are run inside one Neo transaction
+# 
 
-
-describe Neo::Node do
+describe "When using one transactions" do
   before(:all) do
-    Neo::start
+    start
   end
 
   after(:all) do
-    Neo::stop
+    stop
   end  
+  
+  before(:each) do
+    @transaction = Neo::transaction.begin 
+  end
+  
+  after(:each) do
+    @transaction.failure # do not want to store anything
+    @transaction.finish
+  end
 
-  it "should construct a new node in a transaction"  do
-    node = nil
-    Neo::transaction {
+
+# ------------------------------------------------------------------------------
+# NeoService
+# 
+
+  describe Neo::NeoService do
+  
+    it "should not find a meta node of a class that does not exist" do
+      n = Neo::NeoService.instance.find_meta_node('Kalle')
+      n.should be_nil
+    end
+  
+    it "should find the meta node of a class that exists" do
+      class Kalle < Neo::Node 
+      end
+    
+      n = Neo::NeoService.instance.find_meta_node('Kalle')
+      n.should_not be_nil
+      n.should be_kind_of(Neo::MetaNode)
+    end
+ 
+    it "should find an (ruby) object stored in neo given its unique id"
+    #node = Neo::find_node(id) ...
+  
+  end
+
+
+# ------------------------------------------------------------------------------
+# Node
+# 
+  
+  describe Neo::Node do
+ 
+    it "should construct a new node in a transaction"  do
+      node = nil
       node = Neo::Node.new
-    }
-    node.should be_an_instance_of(Neo::Node)
-  end
-
-  it "should run in a transaction if a block is given at new"  do
-    node = Neo::Node.new { }
-    node.should be_an_instance_of(Neo::Node)
-  end
+      node.should be_an_instance_of(Neo::Node)
+    end
   
-  it "should allow to create a node from a native Neo Java object" do
-    node1 = Neo::Node.new { }
-    node2 = Neo::Node.new(node1.internal_node)
-    
-    node1.internal_node.should be_equal(node2.internal_node)
-  end
-  
-  
-  it "should have a unique (neo) id" do
-    n1 = Neo::Node.new{}
-    n1.neo_node_id.should be_kind_of(Fixnum)
-  end
-  
-  
-  it "should know the name of the ruby class it represent" do
-    node1 = Neo::Node.new { }
-    node1.classname.should be == "Neo::Node"
-    
-    class FooBar < Neo::Node
+    it "should run in a transaction if a block is given at new"  do
+      node = Neo::Node.new { }
+      node.should be_an_instance_of(Neo::Node)
     end
     
-    node2 = FooBar.new {}
-    node2.classname.should be == "FooBar"    
-  end
-
-  it "should have a meta node for each class" do
-    class Kalle < Neo::Node 
+    it "should allow to create a node from a native Neo Java object" do
+      node1 = Neo::Node.new { }
+      node2 = Neo::Node.new(node1.internal_node)
+      
+      node1.internal_node.should be_equal(node2.internal_node)
     end
     
-    meta_node = Kalle.meta_node 
-    meta_node.should be_kind_of(Neo::MetaNode)
-  end
-
-  it "should have setter and getters for any property" do
-    #given
-    node = Neo::Node.new do |n|
-      n.foo = "foo"
-      n.bar = "foobar"
+    
+    it "should have a unique (neo) id" do
+      n1 = Neo::Node.new{}
+      n1.neo_node_id.should be_kind_of(Fixnum)
     end
     
-    # then
-    node.foo.should == "foo"
-    node.bar.should == "foobar"
-  end
+    
+    it "should know the name of the ruby class it represent" do
+      node1 = Neo::Node.new { }
+      node1.classname.should be == "Neo::Node"
+      
+      class FooBar < Neo::Node
+      end
+      
+      node2 = FooBar.new {}
+      node2.classname.should be == "FooBar"    
+    end
   
-
-  it "should allow to declare properties"  do
-    # given
-    class Person < Neo::Node
-      properties :name, :age 
+    it "should have a meta node for each class" do
+      class Kalle < Neo::Node 
+      end
+      
+      meta_node = Kalle.meta_node 
+      meta_node.should be_kind_of(Neo::MetaNode)
     end
-    
-    # when
-    person = Person.new {|node|
-      node.name = "kalle"
-      node.age = 42
-    }
-    
-    # then
-    person.name.should == "kalle"
-    person.age.should == 42
-  end
-
-  it "should create a meta node for each new subclass of Node" do
-    # when
-    class FooBar < Neo::Node
-    end
-    
-    # then
-    metanode = Neo::meta_nodes.nodes.find{|node| node.meta_classname == 'FooBar'}
-    metanode.should be_kind_of(Neo::Node)
-  end
   
-
-  it "should have subclasses that have references to a MetaNode" do
-    # when
-    class FooBar < Neo::Node
+    it "should have setter and getters for any property" do
+      #given
+      node = Neo::Node.new do |n|
+        n.foo = "foo"
+        n.bar = "foobar"
+      end
+      
+      # then
+      node.foo.should == "foo"
+      node.bar.should == "foobar"
     end
     
-    # then
-    metanode = Neo::meta_nodes.nodes.find{|node| node.meta_classname == 'FooBar'}
-    metanode.should be_kind_of(Neo::Node)
-  end
   
-  it "should have generated setter and getters for declared properties" do
-    # given
-    class Person < Neo::Node
-      properties :my_property
+    it "should allow to declare properties"  do
+      # given
+      class Person < Neo::Node
+        properties :name, :age 
+      end
+      
+      # when
+      person = Person.new {|node|
+        node.name = "kalle"
+        node.age = 42
+      }
+      
+      # then
+      person.name.should == "kalle"
+      person.age.should == 42
     end
-
-    # when
-    p = Person.new {}
+  
+    it "should have subclasses that have references to a MetaNode" do
+      # when
+      class FooBar3 < Neo::Node
+      end
+      
+      # then
+      metanode = Neo::neo_service.find_meta_node('FooBar3')
+      metanode.should be_kind_of(Neo::MetaNode)
+    end
     
-    # then
-    p.methods.should include("my_property")
-    p.methods.should include("my_property=")
-  end
-
-  it "should have generated setter and getters for subclasses as well" do
-    # given
-    class Person < Neo::Node
-      properties :my_property
+    it "should have generated setter and getters for declared properties" do
+      # given
+      class Person < Neo::Node
+        properties :my_property
+      end
+  
+      # when
+      p = Person.new {}
+      
+      # then
+      p.methods.should include("my_property")
+      p.methods.should include("my_property=")
     end
-
-    class Employee < Person
-      properties :salary
+  
+    it "should have generated setter and getters for subclasses as well" do
+      # given
+      class Person < Neo::Node
+        properties :my_property
+      end
+  
+      class Employee < Person
+        properties :salary
+      end
+  
+      # when
+      p = Employee.new {}
+      
+      # then
+      p.methods.should include("my_property")
+      p.methods.should include("my_property=")
+      p.methods.should include("salary")
+      p.methods.should include("salary=")
     end
-
-    # when
-    p = Employee.new {}
     
-    # then
-    p.methods.should include("my_property")
-    p.methods.should include("my_property=")
-    p.methods.should include("salary")
-    p.methods.should include("salary=")
-  end
+    
+    it "should allow to declare relations" do
+      #given
+      class Person < Neo::Node
+        properties :name, :age 
+        relations :friends
+      end
   
+      person = Person.new    
+      person.methods.should include("friends")
+    end
   
-#  it "should allow to declare relations" do
-#    #given
-#    class Person < Neo::Node
-#      properties :name, :age 
-#      relations :friends
-#    end
-#
-#    p = Person.new {}    
-#    puts "Method: " + p.instance_method.to_s
-#  end
-
     it "should allow to add relations" do
-    #given
-    class Person < Neo::Node
-      properties :name, :age 
-      relations :friends
+      #given
+      class Person < Neo::Node
+        properties :name, :age 
+        relations :friends
+      end
+      
+      p1 = Person.new do|node|
+        node.name = "p1"
+      end
+  
+      # then    
+      p2 = Person.new do|node|
+        node.name = "p2"
+        node.friends << p1
+      end
     end
     
-    p1 = Person.new do|node|
-       node.name = "p1"
-    end
-
-    # then    
-    p2 = Person.new do|node|
-       node.name = "p2"
-       node.friends << p1
-    end
-  end
-  
-  it "should allow to dynamically add relations" do
-    Neo::transaction {
+    it "should allow to dynamically add relations" do
       node1 = Neo::Node.new    
       node2 = Neo::Node.new    
-     
+       
       Neo::Node.add_relation_type(:foos)
-      
+        
       node2.foos << node1
-    }
-  end
-
-
-  it "should have relationship getters that returns Enumerable objects" do
-    #given
-    class Person < Neo::Node
-      properties :name, :age 
-      relations :friends
+    end
+  
+  
+    it "should have relationship getters that returns Enumerable objects" do
+      #given
+      class Person < Neo::Node
+        properties :name, :age 
+        relations :friends
+      end
+      
+      # when
+      p1 = Person.new do |node|
+        node.name = "p1"
+      end
+  
+      p2 = Person.new do |node|
+        node.name = "p2"
+        node.friends << p1
+      end
+      
+      # then
+      p2.friends.should be_kind_of(Enumerable)
+      found = p2.friends.find{|node| node.name == 'p1'}
+      found.name.should == "p1"
+      found.should be_kind_of(Person)
     end
     
-    # when
-    p1 = Person.new do |node|
-       node.name = "p1"
-    end
-
-    p2 = Person.new do |node|
-       node.name = "p2"
-       node.friends << p1
-    end
+    it "should not be possible to add to a relationship a none Neo::Node"
     
-    # then
-    p2.friends.should be_kind_of(Enumerable)
-    found = p2.friends.find{|node| node.name == 'p1'}
-    found.name.should == "p1"
-    found.should be_kind_of(Person)
+    it "should not be possible to set a neo property that is not a string of fixnum"
+    
   end
-  
-  it "should not be possible to add to a relationship a none Neo::Node"
-  
-  it "should not be possible to set a neo property that is not a string of fixnum"
-  
 end
