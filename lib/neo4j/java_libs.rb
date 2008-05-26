50407c3f4c72a14f014d346dc3aa123110c71017
andreasr <andreasr@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2008-05-26 09:04:12 +0000

    rename of folder neo to neo4j
    git-svn-id: https://svn.neo4j.org/laboratory/neo4j.rb@1615 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ lib/neo4j/java_libs.rb
@@ -0,0 +1,16 @@
+include Java
+
+module Neo
+  require 'neo4j/neo-1.0-b6.jar'
+  require 'neo4j/jta-spec1_0_1.jar'
+  require 'neo4j/index-util-0.4-20080512.110337-6.jar'
+  require 'neo4j/lucene-core-2.3.2.jar'
+  
+  EmbeddedNeo = org.neo4j.api.core.EmbeddedNeo
+  StopEvaluator = org.neo4j.api.core.StopEvaluator
+  Traverser = org.neo4j.api.core.Traverser
+  ReturnableEvaluator = org.neo4j.api.core.ReturnableEvaluator
+  Direction = org.neo4j.api.core.Direction
+  IndexService = org.neo4j.util.index.IndexService
+
+end
