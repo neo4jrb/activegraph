9607a3d9111bb2395dd99cd6defb42730a35d916
andreas <andreas@andreas-xps16.(none)>
2009-07-25 10:09:17 +0200

    added contributors file

---
+++ test/replication/master_slave_spec.rb
@@ -0,0 +1,20 @@
+require "spec"
+
+describe "My behaviour" do
+
+  # Called before each example.
+  before(:each) do
+    # Do nothing
+  end
+
+  # Called after each example.
+  after(:each) do
+    # Do nothing
+  end
+
+  it "should do something" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
