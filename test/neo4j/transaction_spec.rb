d8b50e93d9b5188ee6e2fd98b08b5c6d52d4dd1b
andreas <andreas@andreas-xps16.(none)>
2009-09-16 21:27:15 +0200

    Removed dynamic accessor [#67]

--- test/neo4j/transaction_spec.rb
+++ test/neo4j/transaction_spec.rb
@@ -7,7 +7,6 @@ require 'neo4j/spec_helper'
 
 class BaseNode
   include Neo4j::NodeMixin
-  include Neo4j::DynamicAccessorMixin
 end
 
 
@@ -40,33 +39,33 @@ describe "transaction rollback" do
   end
 
   it "should not change properties" do
-    node = Neo4j::Transaction.run { b = BaseNode.new; b.foo = 'foo'; b }
+    node = Neo4j::Transaction.run { b = BaseNode.new; b[:foo] = 'foo'; b }
 
     # when doing a rollback
     Neo4j::Transaction.run { |t|
-      node.foo = "changed"
-      node.foo.should  == "changed"
+      node[:foo] = "changed"
+      node[:foo].should  == "changed"
       t.failure
     }
 
     # then
-    Neo4j::Transaction.run { node.foo.should == 'foo'   }
+    Neo4j::Transaction.run { node[:foo].should == 'foo'   }
   end
 
   it "should rollback a transaction when an exception is thrown"  do
-    node = Neo4j::Transaction.run { b = BaseNode.new; b.foo = 'foo'; b }
+    node = Neo4j::Transaction.run { b = BaseNode.new; b[:foo] = 'foo'; b }
 
     # when doing a rollback
     lambda do
       Neo4j::Transaction.run { |t|
-        node.foo = "changed"
-        node.foo.should  == "changed"
+        node[:foo] = "changed"
+        node[:foo].should  == "changed"
         raise "BOOOM"
       }
     end.should raise_error
     
     # then
-    Neo4j::Transaction.run { node.foo.should == 'foo'   }
+    Neo4j::Transaction.run { node[:foo].should == 'foo'   }
 
   end
 
@@ -90,7 +89,7 @@ describe "When neo has been restarted" do
       node = nil
       Neo4j::Transaction.run {
         node = BaseNode.new
-        node.baaz =  "hello"
+        node[:baaz] =  "hello"
       }
 
       Neo4j.stop
@@ -98,7 +97,7 @@ describe "When neo has been restarted" do
 
       Neo4j::Transaction.run {
         node2 = Neo4j.instance.find_node(node.neo_node_id)
-        node2.baaz.should == "hello"
+        node2[:baaz].should == "hello"
       }
     end
   end
