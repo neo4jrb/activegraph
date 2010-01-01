00a8fd330928d0bcb1377395c56fbfafaac6c6b1
Peter Neubauer <peterneubauer@peter-neubauers-macbook-pro.local>
2010-01-01 22:57:38 +0100

    upgrading to Neo4j 1.0-b11

--- lib/neo4j/jars.rb
+++ lib/neo4j/jars.rb
@@ -1,7 +1,6 @@
 include Java
 
 module Neo4j
-  require 'neo4j/jars/neo-1.0-b10.jar'
-  require 'neo4j/jars/shell-1.0-b10.jar'
-  require 'neo4j/jars/jta-1_1.jar'
+  require 'neo4j/jars/neo-1.0-b11.jar'
+  require 'neo4j/jars/geronimo-jta_1.1_spec-1.1.1.jar'
 end
