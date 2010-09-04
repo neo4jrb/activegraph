193f7d4787045bdf7f3a2c84144a6f4d11c04c1c
Andreas Ronge <andreas.ronge@gmail.com>
2010-09-04 20:46:12 +0200

    Implemented  [] and []= operator on Neo4j::Node

--- lib/neo4j/node.rb
+++ lib/neo4j/node.rb
@@ -1,4 +1,39 @@
 module Neo4j
+
+
+
+  module Property
+
+# Returns true if this property container has a property accessible through the given key, false otherwise.
+    def property?(key)
+      has_property?(key.to_s)
+    end
+
+    # Returns the given property if it exist or nil if it does not exist.
+    def [](key)
+      return unless property?(key)
+      get_property(key.to_s)
+    end
+
+    # Sets the given property to given value.
+    # Will generate an event if the property does not start with '_' (which could be an internal property, like _classname)
+    #
+    def []=(key, value)
+      k = key.to_s
+      if value.nil?
+        delete_property(k)
+      else
+#        value = java.lang.Double.new(value) if value.is_a? Float
+        setProperty(k, value)
+      end
+    end
+  end
+
+  org.neo4j.kernel.impl.core.NodeProxy.class_eval do
+    include Neo4j::Property
+  end
+
+
   class Node
     def self.new(*args)
       # creates a new node using the default db instance when given no args
@@ -26,7 +61,7 @@ module Neo4j
     end
 
     def self.exist?(node_or_node_id, instance = Neo4j.instance)
-      id = node_or_node_id.respond_to?(:id)? node_or_node_id.id : node_or_node_id
+      id = node_or_node_id.respond_to?(:id) ? node_or_node_id.id : node_or_node_id
       self.load(id, instance) != nil
     end
   end
--- spec/behaviour/node_spec.rb
+++ spec/behaviour/node_spec.rb
@@ -2,21 +2,68 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__))
 require 'spec_helper'
 
 describe Neo4j::Node do
-  before(:all) {  FileUtils.rm_rf Neo4j.config[:storage_path]; FileUtils.mkdir_p(Neo4j.config[:storage_path]) }
+  before(:all) { FileUtils.rm_rf Neo4j.config[:storage_path]; FileUtils.mkdir_p(Neo4j.config[:storage_path]) }
   after(:all) { Neo4j.shutdown }
 
-  it "created node should exist in db after transaction finish" do
-    Neo4j::Transaction.new
-    new_node = Neo4j::Node.new
-    Neo4j::Transaction.finish
-    Neo4j::Node.should exist(new_node)
+  describe "Create" do
+    it "created node should exist in db after transaction finish" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      Neo4j::Transaction.finish
+      Neo4j::Node.should exist(new_node)
+    end
+
+    it "created node should exist in db before transaction finish" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      Neo4j::Node.should exist(new_node)
+      Neo4j::Transaction.finish
+    end
   end
 
-  it "created node should exist in db before transaction finish" do
-    Neo4j::Transaction.new
-    new_node = Neo4j::Node.new
-    Neo4j::Node.should exist(new_node)
-    Neo4j::Transaction.finish
-  end
+  describe "Properties" do
+    it "set and get String properties with the [] operator" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      new_node[:key] = 'myvalue'
+      new_node[:key].should == 'myvalue'
+      Neo4j::Transaction.finish
+    end
 
+    it "set and get Fixnum properties with the [] operator" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      new_node[:key] = 42
+      new_node[:key].should == 42
+      Neo4j::Transaction.finish
+    end
+
+
+    it "set and get Float properties with the [] operator" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      new_node[:key] = 3.1415
+      new_node[:key].should == 3.1415
+      Neo4j::Transaction.finish
+    end
+
+    it "set and get Boolean properties with the [] operator" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      new_node[:key] = true
+      new_node[:key].should == true
+      new_node[:key] = false
+      new_node[:key].should == false
+      Neo4j::Transaction.finish
+    end
+
+
+    it "set and get properties with the [] operator and String key" do
+      Neo4j::Transaction.new
+      new_node = Neo4j::Node.new
+      new_node["a"] = 'foo'
+      new_node["a"].should == 'foo'
+      Neo4j::Transaction.finish
+    end
+  end
 end
