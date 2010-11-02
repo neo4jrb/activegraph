64ee3dca1d489cbcee1e36ce628502abf4c0d808
Ben Jackson <ben.jackson1@gmail.com>
2010-11-02 15:52:39 -0400

    Added a model generator for Neo4j

---
+++ lib/generators/neo4j.rb
@@ -0,0 +1,59 @@
+require 'rails/generators/named_base'
+require 'rails/generators/active_model'
+
+module Neo4j
+	module Generators
+	end
+end
+
+class Neo4j::Generators::Base < Rails::Generators::NamedBase #:nodoc:
+	def self.source_root
+		@_neo4j_source_root ||= File.expand_path(File.join(File.dirname(__FILE__),
+			'neo4j', generator_name, 'templates'))
+	end
+end
+
+class Neo4j::Generators::ActiveModel < Rails::Generators::ActiveModel #:nodoc:
+	def self.all(klass)
+		"#{klass}.all"
+	end
+
+	def self.find(klass, params=nil)
+		"#{klass}.find(#{params})"
+	end
+
+	def self.build(klass, params=nil)
+		if params
+			"#{klass}.new(#{params})"
+		else
+			"#{klass}.new"
+		end
+	end
+
+	def save
+		"#{name}.save"
+	end
+
+	def update_attributes(params=nil)
+		"#{name}.update(#{params})"
+	end
+
+	def errors
+		"#{name}.errors"
+	end
+
+	def destroy
+		"#{name}.destroy"
+	end
+end
+
+module Rails
+  module Generators
+    class GeneratedAttribute #:nodoc:
+      def type_class
+        return 'DateTime' if type.to_s == 'datetime'
+        return ":#{type}"
+      end
+    end
+  end
+end
