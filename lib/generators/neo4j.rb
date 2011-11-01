b1f9ae9ba9ae8c3c54f082580f0125bae06d2058
Andreas Ronge <andreas.ronge@gmail.com>
2011-11-01 22:29:48 +0100

    Generators can now generate relationships as well [#195 state:resolved]

--- lib/generators/neo4j.rb
+++ lib/generators/neo4j.rb
@@ -2,63 +2,71 @@ require 'rails/generators/named_base'
 require 'rails/generators/active_model'
 
 module Neo4j
-	module Generators #:nodoc:
-	end
+  module Generators #:nodoc:
+  end
 end
 
 class Neo4j::Generators::Base < Rails::Generators::NamedBase #:nodoc:
-	def self.source_root
-		@_neo4j_source_root ||= File.expand_path(File.join(File.dirname(__FILE__),
-			'neo4j', generator_name, 'templates'))
-	end
+  def self.source_root
+    @_neo4j_source_root ||= File.expand_path(File.join(File.dirname(__FILE__),
+                                                       'neo4j', generator_name, 'templates'))
+  end
 end
 
 class Neo4j::Generators::ActiveModel < Rails::Generators::ActiveModel #:nodoc:
-	def self.all(klass)
-		"#{klass}.all"
-	end
+  def self.all(klass)
+    "#{klass}.all"
+  end
 
-	def self.find(klass, params=nil)
-		"#{klass}.find(#{params})"
-	end
+  def self.find(klass, params=nil)
+    "#{klass}.find(#{params})"
+  end
 
-	def self.build(klass, params=nil)
-		if params
-			"#{klass}.new(#{params})"
-		else
-			"#{klass}.new"
-		end
-	end
+  def self.build(klass, params=nil)
+    if params
+      "#{klass}.new(#{params})"
+    else
+      "#{klass}.new"
+    end
+  end
 
-	def save
-		"#{name}.save"
-	end
+  def save
+    "#{name}.save"
+  end
 
-	def update_attributes(params=nil)
-		"#{name}.update_attributes(#{params})"
-	end
+  def update_attributes(params=nil)
+    "#{name}.update_attributes(#{params})"
+  end
 
-	def errors
-		"#{name}.errors"
-	end
+  def errors
+    "#{name}.errors"
+  end
 
-	def destroy
-		"#{name}.destroy"
-	end
+  def destroy
+    "#{name}.destroy"
+  end
 end
 
 module Rails
   module Generators
     class GeneratedAttribute #:nodoc:
       def type_class
-      	case type.to_s.downcase
-					when 'datetime' 										then 'DateTime'
-					when 'date' 												then 'Date'
-					when 'text' 												then 'String'
-					when 'integer', 'number', 'fixnum' 	then 'Fixnum'
-					when 'float' 												then 'Float'
-					else 'String'
-      	end
+        case type.to_s.downcase
+          when 'any' then
+            'any'
+          when 'datetime' then
+            'DateTime'
+          when 'date' then
+            'Date'
+          when 'text' then
+            'String'
+          when 'integer', 'number', 'fixnum' then
+            'Fixnum'
+          when 'float' then
+            'Float'
+          else
+            'String'
+        end
       end
     end
   end
