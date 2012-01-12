c3e0dd51b2b1e2062082d3ee5b0f7786fa11804f
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-12 19:31:09 +1100

    use shoulda-matchers, manual ActiveModel Lint-in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,9 @@ begin
   require 'rspec'
   require 'fileutils'
   require 'tmpdir'
-  require 'rspec-rails-matchers'
+  require 'active_model' # Need to pull this before shoulda-matcher, so it can configure RSpec
+  require 'shoulda-matchers'
+  require 'shoulda/matchers/active_model'
   require 'benchmark'
   require 'pry'
 
