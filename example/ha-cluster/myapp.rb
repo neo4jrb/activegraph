d0c45518182bc334a81c14f18d5497d757a902db
Andreas Ronge <andreas.ronge@gmail.com>
2012-01-14 15:47:55 +0100

    Fixing some issues with the ha-cluster example (still not working) #115
    Unsolved problems:
    * Need to extend different (private !) java classes since the internal structure of the neo4j enterprise has changed
    * Eventhandling does not work together with neo4j.rb in HA using the latest 1.6.M02 (undefined method 'size')

--- example/ha-cluster/myapp.rb
+++ example/ha-cluster/myapp.rb
@@ -8,7 +8,7 @@ def start(machine_id)
   Neo4j::Config.default_file = 'config.yml' # use local config file
 
   # override this default config with this machine configuration
-  Neo4j.config['ha.machine_id'] = machine_id
+  Neo4j.config['ha.server_id'] = machine_id
   Neo4j.config['ha.server'] = "localhost:600#{machine_id}"
   Neo4j.config['enable_remote_shell'] = "port=933#{machine_id}"
 
