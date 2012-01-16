1a72e4a541a8ab5be276f17bba9b46abef1a1ca5
Andreas Ronge <andreas.ronge@gmail.com>
2012-01-16 13:02:16 +0100

    Added spec for backups (which needs enterprise edition) #115

---
+++ spec/backup_spec.rb
@@ -0,0 +1,30 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+
+describe Neo4j, " with neo4j-enterprise edition", :edition => :enterprise do
+
+  describe "Backup enabled" do
+    after(:all) do
+      Neo4j.shutdown
+    end
+
+    it "can do a backup" do
+      Neo4j::Config['enable_online_backup'] = 'true'
+      backup_source_dir = File.join(Dir.tmpdir, "neo4j_backup_source")
+      backup_target_dir = File.join(Dir.tmpdir, "neo4j_backup_target")
+      FileUtils.rm_rf backup_source_dir
+      FileUtils.rm_rf backup_target_dir
+
+      Thread.new do
+        Neo4j::Config[:storage_path] = backup_source_dir
+        Neo4j.start
+        Neo4j::Config[:storage_path] = File.join(Dir.tmpdir, "neo4j-rspec-db")
+        Neo4j::Transaction.run { Neo4j.ref_node[:ko] = 'fo' }
+        5.times { puts "Sleep"; sleep 1 }
+      end
+      sleep 2 # make sure neo4j starts
+      Neo4j::Config[:storage_path] = File.join(Dir.tmpdir, "neo4j-rspec-db")
+      org.neo4j.backup.OnlineBackup.from('localhost').full(backup_target_dir)
+      File.exist?(backup_target_dir).should be_true
+    end
+  end
+end
