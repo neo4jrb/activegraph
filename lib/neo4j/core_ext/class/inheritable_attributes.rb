ec1560a34064d94d339d3aa9ae150743f444eea6
Dmytrii Nagirniak <dnagir@gmail.com>
2012-01-16 15:06:03 +1100

    trying to update to Rails 3.2

--- lib/neo4j/core_ext/class/inheritable_attributes.rb
+++ lib/neo4j/core_ext/class/inheritable_attributes.rb
@@ -1,37 +1,19 @@
 # This is copied from Rails Active Support since it has been depricated and I still need it
-class Class # :nodoc:
-  def class_inheritable_reader(*syms)
-    options = syms.extract_options!
-    syms.each do |sym|
-      next if sym.is_a?(Hash)
-      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
-        def self.#{sym}                                # def self.after_add
-          read_inheritable_attribute(:#{sym})          #   read_inheritable_attribute(:after_add)
-        end                                            # end
-                                                       #
-        #{"                                            #
-        def #{sym}                                     # def after_add
-          self.class.#{sym}                            #   self.class.after_add
-        end                                            # end
-        " unless options[:instance_reader] == false }  # # the reader above is generated unless options[:instance_reader] == false
-      EOS
-    end
-  end
 
-  def class_inheritable_writer(*syms)
-    options = syms.extract_options!
-    syms.each do |sym|
-      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
-        def self.#{sym}=(obj)                          # def self.color=(obj)
-          write_inheritable_attribute(:#{sym}, obj)    #   write_inheritable_attribute(:color, obj)
-        end                                            # end
-                                                       #
-        #{"                                            #
-        def #{sym}=(obj)                               # def color=(obj)
-          self.class.#{sym} = obj                      #   self.class.color = obj
-        end                                            # end
-        " unless options[:instance_writer] == false }  # # the writer above is generated unless options[:instance_writer] == false
-      EOS
-    end
-  end
+# Taken from: https://raw.github.com/lifo/docrails/dd6c3676af3fa6019c53a59f62c4fd14966be728/activesupport/lib/active_support/core_ext/class/inheritable_attributes.rb
+
+# Changes made:
+# - Remove deprecation warnings
+# - Ignore if already available from ActiveSupport
+
+# TODO: convert the code to use class_attribute. dnagir failed at it :)
+
+
+
+begin
+  require 'active_support/core_ext/class/inheritable_attributes'
+rescue LoadError
+ensure
+  # Fallback to internal implementation & override deprecations
+  require 'neo4j/core_ext/class/rewrite_inheritable_attributes.rb'
 end
