2ce3df9b7727a963d0b2adbe24fdd47f662f74c6
Andreas Ronge <andreas.ronge@gmail.com>
2012-02-17 08:38:41 +0100

    Revert "stringify keys for cypher subtitution", broken RSpecs
    This reverts commit 1e89b1d2699e4adcfd7c5ea814030a12bfcd5f21.

--- lib/neo4j/neo4j.rb
+++ lib/neo4j/neo4j.rb
@@ -86,7 +86,6 @@ module Neo4j
     #
     def query(query, params = {})
       engine = org.neo4j.cypher.javacompat.ExecutionEngine.new(db)
-      params.stringify_keys!
       engine.execute(query, params)
     end
 
