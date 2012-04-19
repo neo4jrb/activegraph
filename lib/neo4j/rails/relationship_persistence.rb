abfeb709760f7432f1ee59723cd07082124e150e
Andreas Ronge <andreas.ronge@gmail.com>
2012-04-19 19:02:07 +0200

    Made start_node= and end_node= protected closes 186

--- lib/neo4j/rails/relationship_persistence.rb
+++ lib/neo4j/rails/relationship_persistence.rb
@@ -34,10 +34,12 @@ module Neo4j
         true
       end
 
+      # @return [Symbol] the relationship type
       def rel_type
         new_record? ? @_rel_type : _java_entity.rel_type.to_sym
       end
 
+      # @see http://rdoc.info/github/andreasronge/neo4j-core/Neo4j/Core/Relationship#other_node-instance_method
       def other_node(node)
         if persisted?
           _java_rel._other_node(node._java_node)
@@ -46,38 +48,16 @@ module Neo4j
         end
       end
 
-
+      # Returns the start node which can be unpersisted
+      # @see http://rdoc.info/github/andreasronge/neo4j-core/Neo4j/Core/Relationship#start_node-instance_method
       def start_node
         @_start_node ||= _java_rel && _java_rel.start_node.wrapper
       end
 
-      def start_node=(node)
-        old = @_start_node
-        @_start_node = node
-        # TODO should raise exception if not persisted and changed
-        if old != @_start_node
-          old && old.rm_outgoing_rel(rel_type, self)
-          @_start_node.class != Neo4j::Node && @_start_node.add_outgoing_rel(rel_type, self)
-        end
-      end
-
+      # Returns the end node which can be unpersisted
+      # @see http://rdoc.info/github/andreasronge/neo4j-core/Neo4j/Core/Relationship#end_node-instance_method
       def end_node
         @_end_node ||= _java_rel && _java_rel.end_node.wrapper
-      end
-
-      def end_node=(node)
-        old = @_end_node
-        @_end_node = node
-        # TODO should raise exception if not persisted and changed
-        if old != @_end_node
-          old && old.rm_incoming_rel(rel_type, self)
-          @_end_node.class != Neo4j::Node && @_end_node.add_incoming_rel(rel_type, self)
-        end
-      end
-
-
-      def _persist_node(start_or_end_node)
-        (start_or_end_node.new_record? || start_or_end_node.relationships_changed?) ? start_or_end_node.save : true
       end
 
       # Reload the object from the DB
@@ -100,6 +80,33 @@ module Neo4j
         end
         reloaded
       end
+
+      protected
+
+      def start_node=(node)
+        old = @_start_node
+        @_start_node = node
+        # TODO should raise exception if not persisted and changed
+        if old != @_start_node
+          old && old.rm_outgoing_rel(rel_type, self)
+          @_start_node.class != Neo4j::Node && @_start_node.add_outgoing_rel(rel_type, self)
+        end
+      end
+
+      def end_node=(node)
+        old = @_end_node
+        @_end_node = node
+        # TODO should raise exception if not persisted and changed
+        if old != @_end_node
+          old && old.rm_incoming_rel(rel_type, self)
+          @_end_node.class != Neo4j::Node && @_end_node.add_incoming_rel(rel_type, self)
+        end
+      end
+
+      def _persist_node(start_or_end_node)
+        (start_or_end_node.new_record? || start_or_end_node.relationships_changed?) ? start_or_end_node.save : true
+      end
+
     end
 
   end
