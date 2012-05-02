799cdf9477bf3c3f1b0a8e95f5b0caca7d11e879
Andreas Ronge <andreas.ronge@gmail.com>
2012-05-02 14:07:15 +0200

    Added missing Neo4j::VERSION constant

--- lib/neo4j.rb
+++ lib/neo4j.rb
@@ -12,6 +12,7 @@ require 'active_support/core_ext/class/attribute'
 
 require 'active_model'
 
+require 'neo4j/version'
 require 'neo4j-core'
 require 'neo4j-wrapper'
 
@@ -20,4 +21,3 @@ require 'neo4j/rails/rails'
 require 'neo4j/paginated'
 
 require 'orm_adapter/adapters/neo4j'
-Dir["#{File.dirname(__FILE__)}/tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake) && respond_to?(:namespace)
