e81de3fcf74e900981d9cb14d2bfa1da0416dbf8
Andreas Ronge <andreas.ronge@gmail.com>
2014-01-20 22:23:12 +0100

    Fixing template for 3.x version, new template: rails new myapp -m http://andreasronge.github.com/neo4j/neo4j.rb -O

---
+++ neo4j.rb
@@ -0,0 +1,20 @@
+# Usage: rails new myapp -m neo4j.rb -O
+
+#NEO4J_VERSION = "3.0.0.alpha.2"
+
+gem "neo4j", '3.0.0.alpha.2'
+
+
+generator = %q[
+    config.generators do |g|
+      g.orm             :neo4j
+    end
+
+    # Configure where the embedded neo4j database should exist
+    # Notice embedded db is only available for JRuby
+    # config.neo4j.session_type = :embedded_db  # default #server_db
+    # config.neo4j.session_path = File.expand_path('neo4j-db', Rails.root)
+]
+
+application generator
+inject_into_file 'config/application.rb', "\nrequire 'neo4j/railtie'", :after => 'require "sprockets/railtie"'
