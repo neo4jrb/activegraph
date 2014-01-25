129e4a0a3f6a6bdaa3cf137e42a426d5c9a9040c
Andreas Ronge <andreas.ronge@gmail.com>
2014-01-25 18:55:44 +0100

    Impl rails scaffolding templates #302

---
+++ lib/rails/generators/neo4j/model/model_generator.rb
@@ -0,0 +1,79 @@
+require File.join(File.dirname(__FILE__), '..', '..', '..', '..','neo4j.rb')
+
+class Neo4j::Generators::ModelGenerator < Neo4j::Generators::Base #:nodoc:
+  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+
+  check_class_collision
+
+  class_option :timestamps, :type => :boolean
+  class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
+  class_option :indices,    :type => :array,  :desc => "The properties which should be indexed"
+  class_option :has_one,    :type => :array,  :desc => "A list of has_one relationships"
+  class_option :has_n,      :type => :array,  :desc => "A list of has_n relationships"
+
+  def create_model_file
+    template "model.erb", File.join('app/models', "#{singular_name}.rb")
+  end
+
+  protected
+  def migration?
+    false
+  end
+
+  def timestamps?
+    options[:timestamps]
+  end
+
+  def has_n?
+    options[:has_n]
+  end
+
+  def has_n_statements
+    txt = ""
+    options[:has_n].each do |key|
+      to, from = key.split(':')
+      txt << (from ? "\n  has_n(:#{to}).from(:#{from})\n" : "\n  has_n :#{to}")
+    end
+    txt
+  end
+
+  def has_one?
+    options[:has_one]
+  end
+
+  def has_one_statements
+    txt = ""
+    options[:has_one].each do |key|
+      to, from = key.split(':')
+      txt << (from ? "\n  has_one(:#{to}).from(:#{from})\n" : "\n  has_one :#{to}")
+    end
+    txt
+  end
+
+  def indices?
+    options[:indices]
+  end
+
+
+  def index_fragment(property)
+    if options[:indices] && options[:indices].include?(property)
+      "index :#{property}"
+    end
+  end
+
+  def parent?
+    options[:parent]
+  end
+
+  def timestamp_statements
+    %q{
+  property :created_at, :type => DateTime
+  # property :created_on, :type => Date
+
+  property :updated_at, :type => DateTime
+  # property :updated_on, :type => Date
+}
+  end
+
+  hook_for :test_framework
+end
---
+++ lib/rails/generators/neo4j_generator.rb
@@ -0,0 +1,74 @@
+require 'rails/generators/named_base'
+require 'rails/generators/active_model'
+
+
+module Neo4j
+  module Generators #:nodoc:
+  end
+end
+
+class Neo4j::Generators::Base < ::Rails::Generators::NamedBase #:nodoc:
+  def self.source_root
+    @_neo4j_source_root ||= File.expand_path(File.join(File.dirname(__FILE__),
+                                                       'neo4j', generator_name, 'templates'))
+  end
+end
+
+class Neo4j::Generators::ActiveModel < Rails::Generators::ActiveModel #:nodoc:
+  def self.all(klass)
+    "#{klass}.all"
+  end
+
+  def self.find(klass, params=nil)
+    "#{klass}.find(#{params})"
+  end
+
+  def self.build(klass, params=nil)
+    if params
+      "#{klass}.new(#{params})"
+    else
+      "#{klass}.new"
+    end
+  end
+
+  def save
+    "#{name}.save"
+  end
+
+  def update_attributes(params=nil)
+    "#{name}.update_attributes(#{params})"
+  end
+
+  def errors
+    "#{name}.errors"
+  end
+
+  def destroy
+    "#{name}.destroy"
+  end
+end
+
+module Rails
+  module Generators
+    class GeneratedAttribute #:nodoc:
+      def type_class
+        case type.to_s.downcase
+          when 'any' then
+            'any'
+          when 'datetime' then
+            'DateTime'
+          when 'date' then
+            'Date'
+          when 'text' then
+            'String'
+          when 'integer', 'number', 'fixnum' then
+            'Fixnum'
+          when 'float' then
+            'Float'
+          else
+            'String'
+        end
+      end
+    end
+  end
+end
