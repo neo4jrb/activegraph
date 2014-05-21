cf276e210f7fb226900c67e00a0407d4b028ea68
Brian Underwood <brianu@goodeggs.com>
2014-05-20 22:07:43 -0700

    Need to switch order of Validations and Callbacks modules so that valid? calls both run_validations! methods in ActiveModel

--- lib/neo4j/active_node.rb
+++ lib/neo4j/active_node.rb
@@ -33,8 +33,8 @@ module Neo4j
     include Neo4j::ActiveNode::Persistence
     include Neo4j::ActiveNode::Property
     include Neo4j::ActiveNode::Labels
-    include Neo4j::ActiveNode::Callbacks
     include Neo4j::ActiveNode::Validations
+    include Neo4j::ActiveNode::Callbacks
     include Neo4j::ActiveNode::Rels
     include Neo4j::ActiveNode::HasN
 
