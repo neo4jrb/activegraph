9a5ec78b3ba299046d0636e8732cd2751c14db22
Brian Underwood <brianu@goodeggs.com>
2014-05-23 11:31:31 -0700

    Pull out writer methods on initialize and set the values on the model

--- lib/neo4j/active_node/property.rb
+++ lib/neo4j/active_node/property.rb
@@ -12,7 +12,14 @@ module Neo4j::ActiveNode
     end
 
     def initialize(attributes={}, options={})
+      relationship_props = self.class.extract_relationship_attributes!(attributes)
+      writer_method_props = extract_writer_methods!(attributes)
+
       validate_attributes!(attributes)
+
+      writer_method_props.each do |key, value|
+        self.send("#{key}=", value)
+      end
 
       super(attributes, options)
       (@changed_attributes || {}).clear
@@ -28,13 +35,16 @@ module Neo4j::ActiveNode
     # Changes attributes hash to remove relationship keys
     # Raises an error if there are any keys left which haven't been defined as properties on the model
     def validate_attributes!(attributes)
-      relationship_props = self.class.extract_relationship_attributes!(attributes)
-
-      invalid_properties = (attributes.keys.map(&:to_s) - self.attributes.keys).reject do |attribute|
-        self.respond_to?("#{attribute}=")
-      end
-
+      invalid_properties = attributes.keys.map(&:to_s) - self.attributes.keys
       raise UndefinedPropertyError, "Undefined properties: #{invalid_properties.join(',')}" if invalid_properties.size > 0
+    end
+
+    def extract_writer_methods!(attributes)
+      attributes.keys.inject({}) do |writer_method_props, key|
+        writer_method_props[key] = attributes.delete(key) if self.respond_to?("#{key}=")
+
+        writer_method_props
+      end
     end
 
     module ClassMethods
