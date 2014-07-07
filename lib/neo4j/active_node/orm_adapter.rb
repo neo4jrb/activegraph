0f495cd2aaca6c59879525b0dd89bcaecff3b5cf
Brian Underwood <ml+github@semi-sentient.com>
2014-07-07 09:22:03 -0700

    Use neo_id key on conditions to get ID(n) WHERE condition

--- lib/neo4j/active_node/orm_adapter.rb
+++ lib/neo4j/active_node/orm_adapter.rb
@@ -73,7 +73,7 @@ module Neo4j
 
       def extract_id!(conditions)
         if id = conditions.delete(:id)
-          conditions['id(n)'] = id.to_i
+          conditions[:neo_id] = id.to_i
         end
       end
 
