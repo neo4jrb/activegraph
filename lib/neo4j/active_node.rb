4116db7059c45a6b479a9fe5ecff29203708b38b
Brian Underwood <ml+github@semi-sentient.com>
2014-07-14 19:52:03 -0700

    Load the Query module first so that the #first method gets overwritten properly

--- lib/neo4j/active_node.rb
+++ lib/neo4j/active_node.rb
@@ -33,12 +33,12 @@ module Neo4j
     include Neo4j::ActiveNode::Identity
     include Neo4j::ActiveNode::Persistence
     include Neo4j::ActiveNode::Property
+    include Neo4j::ActiveNode::Query
     include Neo4j::ActiveNode::Labels
     include Neo4j::ActiveNode::Validations
     include Neo4j::ActiveNode::Callbacks
     include Neo4j::ActiveNode::Rels
     include Neo4j::ActiveNode::HasN
-    include Neo4j::ActiveNode::Query
 
     def wrapper
       self
