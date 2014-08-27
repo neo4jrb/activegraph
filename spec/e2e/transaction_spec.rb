427ef82ab9be8692f125b9ff147c66d3126c0e06
Andreas Ronge <andreas.ronge@gmail.com>
2014-08-27 11:04:38 +0200

    updated test to make it work with neo4j embedded, issue #440

--- spec/e2e/transaction_spec.rb
+++ spec/e2e/transaction_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Neo4j::Transaction' do
-  context 'reading has_one relationships' do
+  context 'reading has_one relationships for Neo4j::Server' do
     let(:clazz) do
       UniqueClass.create do
         include Neo4j::ActiveNode
@@ -11,13 +11,27 @@ describe 'Neo4j::Transaction' do
     end
 
     it 'returns  hash values inside but outside it has the node value after commit' do
-      tx = Neo4j::Transaction.new
-      a = clazz.create name: 'a'
-      b = clazz.create name: 'b'
-      a.thing = b
-      expect(a.thing).to eq("name"=>"b", "_classname"=>clazz.to_s)
-      tx.close
-      expect(a.thing).to eq(b)
+
+      if Neo4j::Session.current.db_type == :server_db
+        tx = Neo4j::Transaction.new
+        a = clazz.create name: 'a'
+        b = clazz.create name: 'b'
+        a.thing = b
+        expect(a.thing).to eq("name"=>"b", "_classname"=>clazz.to_s)
+        tx.close
+        expect(a.thing).to eq(b)
+      end
+
+      if Neo4j::Session.current.db_type == :embedded_db
+        tx = Neo4j::Transaction.new
+        a = clazz.create name: 'a'
+        b = clazz.create name: 'b'
+        a.thing = b
+        expect(a.thing).to eq(b)
+        tx.close
+        expect(a.thing).to eq(b)
+      end
+
     end
   end
 end
