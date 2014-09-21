4c821f868d45396b7040385024c3d60a2f9cb88f
Brian Underwood <ml+github@semi-sentient.com>
2014-09-21 13:48:49 +0200

    Allow for configuration of the batch size and output time per node so that users can tune to their DBs

--- lib/neo4j/migration.rb
+++ lib/neo4j/migration.rb
@@ -12,6 +12,8 @@ module Neo4j
         puts "This task will add an ID Property every node in the given file."
         puts "It may take a significant amount of time, please be patient."
         models.each do |model|
+          puts
+          puts
           puts "Adding IDs to #{model}"
           add_ids_to model.constantize
         end
@@ -29,24 +31,35 @@ module Neo4j
       private
 
       def add_ids_to(model)
+        require 'benchmark'
+
+        max_per_batch = (ENV['MAX_PER_BATCH'] || max_per_batch).to_i
+
         label = model.mapped_label_name
         property = model.primary_key
         total = 1
+        last_time_taken = nil
 
         until total == 0
-          total = Neo4j::Session.query("MATCH (n:`#{label}`) WHERE NOT has(n.#{property}) RETURN COUNT(n) as ids").first.ids
+          total = Neo4j::Session.query.match(n: label).where("NOT has(n.#{property})").return("COUNT(n) AS ids").first.ids
+
+          time_per_node = last_time_taken / max_per_batch if last_time_taken
+          print "#{total} nodes left.  Last batch: #{time_per_node && (time_per_node * 1000.0).round(1)}ms / node\r"
+
           return if total == 0
           to_set = total > max_per_batch ? max_per_batch : total
           new_ids = [].tap do | ids_array|
                       to_set.times { ids_array.push "'#{new_id_for(model)}'" }
                     end
-          Neo4j::Session.query("MATCH (n:`#{label}`) WHERE NOT has(n.#{property})
-            with COLLECT(n) as nodes, [#{new_ids.join(',')}] as ids
-            FOREACH(i in range(0,#{to_set - 1})| 
-              FOREACH(node in [nodes[i]]|
-                SET node.#{property} = ids[i]))
-            RETURN distinct(true)
-            limit #{to_set}")
+          last_time_taken = Benchmark.realtime do
+            Neo4j::Session.query("MATCH (n:`#{label}`) WHERE NOT has(n.#{property})
+              with COLLECT(n) as nodes, [#{new_ids.join(',')}] as ids
+              FOREACH(i in range(0,#{to_set - 1})| 
+                FOREACH(node in [nodes[i]]|
+                  SET node.#{property} = ids[i]))
+              RETURN distinct(true)
+              limit #{to_set}")
+          end
         end
       end
 
