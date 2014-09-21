de559ba73688c3033909e5364466a3b41e45de89
Brian Underwood <ml+github@semi-sentient.com>
2014-09-21 10:01:26 +0200

    UUIDs aren't created as sorted

--- spec/integration/label_spec.rb
+++ spec/integration/label_spec.rb
@@ -178,13 +178,13 @@ describe "Labels" do
 
     describe 'first' do
       it 'returns the first object created... sort of, see docs' do
-        expect(FirstLastTestClass.first).to eq @jasmine
+        expect(FirstLastTestClass.first).to eq [@jasmine, @lauren].sort_by(&:uuid).first
       end
     end
 
     describe 'last' do
       it 'returns the last object created... sort of, see docs' do
-        expect(FirstLastTestClass.last).to eq @lauren
+        expect(FirstLastTestClass.last).to eq [@jasmine, @lauren].sort_by(&:uuid).last
       end
 
       it 'returns nil when there are no results' do
