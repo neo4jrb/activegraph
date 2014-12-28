8cd33fb0df7bce1ecd2fff8092b3fe7c5bc889ec
Brian Underwood <ml+github@semi-sentient.com>
2014-12-28 15:11:05 +0100

    Rubocop: Style/CaseIndentation

--- lib/rails/generators/neo4j_generator.rb
+++ lib/rails/generators/neo4j_generator.rb
@@ -53,20 +53,20 @@ module Rails
     class GeneratedAttribute #:nodoc:
       def type_class
         case type.to_s.downcase
-          when 'any' then
-            'any'
-          when 'datetime' then
-            'DateTime'
-          when 'date' then
-            'Date'
-          when 'text' then
-            'String'
-          when 'integer', 'number', 'fixnum' then
-            'Integer'
-          when 'float' then
-            'Float'
-          else
-            'String'
+        when 'any' then
+          'any'
+        when 'datetime' then
+          'DateTime'
+        when 'date' then
+          'Date'
+        when 'text' then
+          'String'
+        when 'integer', 'number', 'fixnum' then
+          'Integer'
+        when 'float' then
+          'Float'
+        else
+          'String'
         end
       end
     end
