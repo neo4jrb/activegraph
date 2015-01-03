30963e0f5e0905e8e13485e4e1787d8ed1073758
Chris Grigg <chris@subvertallmedia.com>
2015-01-03 15:44:08 -0500

    DRY association setup

--- lib/neo4j/active_node/has_n.rb
+++ lib/neo4j/active_node/has_n.rb
@@ -88,14 +88,7 @@ module Neo4j::ActiveNode
       # rubocop:disable Style/PredicateName
       def has_many(direction, name, options = {})
         name = name.to_sym
-
-        association = Neo4j::ActiveNode::HasN::Association.new(:has_many, direction, name, options)
-        @associations ||= {}
-        @associations[name] = association
-
-        target_class_name = association.target_class_name || 'nil'
-        create_reflection(:has_many, name, association, self)
-
+        association, target_class_name = builder(:has_many, direction, name, options)
         # TODO: Make assignment more efficient? (don't delete nodes when they are being assigned)
         module_eval(%{
           def #{name}(node = nil, rel = nil)
@@ -146,13 +139,7 @@ module Neo4j::ActiveNode
 
       def has_one(direction, name, options = {})
         name = name.to_sym
-
-        association = Neo4j::ActiveNode::HasN::Association.new(:has_one, direction, name, options)
-        @associations ||= {}
-        @associations[name] = association
-
-        target_class_name = association.target_class_name || 'nil'
-        create_reflection(:has_one, name, association, self)
+        association, target_class_name = builder(:has_one, direction, name, options)
 
         module_eval(%{
           def #{name}=(other_node)
@@ -191,6 +178,18 @@ module Neo4j::ActiveNode
           end}, __FILE__, __LINE__)
       end
       # rubocop:enable Style/PredicateName
+
+      private
+
+      def builder(macro, direction, name, options)
+        association = Neo4j::ActiveNode::HasN::Association.new(macro, direction, name, options)
+        @associations ||= {}
+        @associations[name] = association
+
+        target_class_name = association.target_class_name || 'nil'
+        create_reflection(macro, name, association, self)
+        [association, target_class_name]
+      end
     end
   end
 end
