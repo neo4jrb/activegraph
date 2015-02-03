8474b8def3b43ba6375221a41fa93d283a23c772
Brian Underwood <ml+github@semi-sentient.com>
2015-02-03 10:10:47 +0200

    Rubocop: Style/ClassVars

--- spec/unique_class.rb
+++ spec/unique_class.rb
@@ -1,27 +1,29 @@
 module UniqueClass
-  @@_counter = 1
+  @counter = 1
 
-  def self._unique_random_number
-    "#{Time.now.year}#{Time.now.to_i}#{Time.now.usec.to_s[0..2]}".to_i
-  end
+  class << self
+    def _unique_random_number
+      "#{Time.now.year}#{Time.now.to_i}#{Time.now.usec.to_s[0..2]}".to_i
+    end
 
-  def self.set(klass, name = nil)
-    name ||= "Model_#{@@_counter}_#{_unique_random_number}"
-    @@_counter += 1
-    klass.class_eval <<-RUBY
-  def self.to_s
-    "#{name}"
-  end
-    RUBY
-    # Object.send(:remove_const, name) if Object.const_defined?(name)
-    Object.const_set(name, klass) # unless Kernel.const_defined?(name)
-    klass
-  end
+    def set(klass, name = nil)
+      name ||= "Model_#{@counter}_#{_unique_random_number}"
+      @counter += 1
+      klass.class_eval <<-RUBY
+        def self.to_s
+          "#{name}"
+        end
+      RUBY
+      # Object.send(:remove_const, name) if Object.const_defined?(name)
+      Object.const_set(name, klass) # unless Kernel.const_defined?(name)
+      klass
+    end
 
-  def self.create(class_name = nil, &block)
-    clazz = Class.new
-    set(clazz, class_name)
-    clazz.class_eval(&block) if block
-    clazz
+    def create(class_name = nil, &block)
+      clazz = Class.new
+      set(clazz, class_name)
+      clazz.class_eval(&block) if block
+      clazz
+    end
   end
 end
