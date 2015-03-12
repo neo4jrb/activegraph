f5cddd551167a8cb3d2de64e2bf1ce6462ef020b
Brian Underwood <ml+github@semi-sentient.com>
2015-03-12 12:31:51 +0200

    Try removing this extension as well

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,6 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
