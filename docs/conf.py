44100fc91fb35ec0ef02ba04fa48c662bc20f244
Brian Underwood <ml+github@semi-sentient.com>
2015-03-12 12:30:50 +0200

    Maybe it's the ruby domain?

--- docs/conf.py
+++ docs/conf.py
@@ -29,8 +29,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.todo',
-    'sphinxcontrib.rubydomain'
+    'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
