d1d7d9090db376c9456765d4d4576e1ab7d3ff95
Brian Underwood <ml+github@semi-sentient.com>
2015-03-25 18:49:43 +0100

    Rubocop fix

--- docs/_yard/custom_templates/default/fulldoc/setup.rb
+++ docs/_yard/custom_templates/default/fulldoc/setup.rb
@@ -9,10 +9,9 @@ def render_node(object, ancestry = [])
   FileUtils.mkdir_p(path) unless path.empty?
   asset File.join(path, "#{object.name}.rst"), erb(:module)
 
-  if object.respond_to?(:children)
-    object.children.each do |child|
-      render_node(child, ancestry + [object]) if child.is_a?(YARD::CodeObjects::NamespaceObject)
-    end
+  return if !object.respond_to?(:children)
+
+  object.children.each do |child|
+    render_node(child, ancestry + [object]) if child.is_a?(YARD::CodeObjects::NamespaceObject)
   end
 end
-
