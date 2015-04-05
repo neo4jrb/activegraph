31859bcfc7fb3c7e924a26156f888e9d1c7e40c4
Brian Underwood <ml+github@semi-sentient.com>
2015-04-05 16:18:34 +0200

    Do this to fix jruby for now

--- spec/e2e/node_wrapping_spec.rb
+++ spec/e2e/node_wrapping_spec.rb
@@ -47,13 +47,15 @@ describe 'Node Wrapping' do
 
     let(:result) { Neo4j::Session.query.match("(n#{label_string})").pluck(:n).first }
 
+    raw_node_class = (RUBY_PLATFORM =~ /java/) ? 'Java::OrgNeo4jKernelImplCore::NodeProxy' : '::Neo4j::Server::CypherNode'
+
     {
-      %w(ExtraneousLabel) => '::Neo4j::Node',
+      %w(ExtraneousLabel) => raw_node_class,
       %w(Post) => 'Post',
 
       %w(ExtraneousLabel Post) => 'Post',
 
-      %w(SomeOtherClass) => '::Neo4j::Node',
+      %w(SomeOtherClass) => raw_node_class,
       %w(SomeOtherClass Post) => 'Post',
 
       %w(User GitHub) => 'GitHubUser',
