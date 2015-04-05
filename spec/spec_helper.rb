d306911f08309e65656d487697d0c8f1047f61db
Brian Underwood <ml+github@semi-sentient.com>
2015-04-05 13:46:16 +0200

    Rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -108,7 +108,7 @@ def clear_model_memory_caches
 end
 
 def delete_db
-  #clear_model_memory_caches
+  # clear_model_memory_caches
   Neo4j::Session.current._query('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
 end
 
