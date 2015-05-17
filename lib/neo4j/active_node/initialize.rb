3b9b9ce31eb74539dce9e6322632ecf62b96875a
Chris Grigg <chris@subvertallmedia.com>
2015-05-16 22:08:55 -0400

    split attribute processing out of :init_on_load

--- lib/neo4j/active_node/initialize.rb
+++ lib/neo4j/active_node/initialize.rb
@@ -9,10 +9,7 @@ module Neo4j::ActiveNode::Initialize
     self.class.extract_association_attributes!(properties)
     @_persisted_obj = persisted_node
     changed_attributes && changed_attributes.clear
-    @attributes ||= self.class.attributes_nil_hash.dup
-    stringify_attributes!(@attributes, properties)
-    self.default_properties = properties
-    @attributes = self.class.declared_property_manager.convert_properties_to(self, :ruby, @attributes)
+    @attributes = convert_and_assign_attributes(properties)
   end
 
   # Implements the Neo4j::Node#wrapper and Neo4j::Relationship#wrapper method
@@ -23,6 +20,13 @@ module Neo4j::ActiveNode::Initialize
   end
 
   private
+
+  def convert_and_assign_attributes(properties)
+    @attributes ||= self.class.attributes_nil_hash.dup
+    stringify_attributes!(@attributes, properties)
+    self.default_properties = properties
+    self.class.declared_property_manager.convert_properties_to(self, :ruby, @attributes)
+  end
 
   def stringify_attributes!(attr, properties)
     properties.each_pair do |k, v|
