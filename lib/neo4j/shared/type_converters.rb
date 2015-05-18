cefa25b03185e2bbb922647b3f5d48f4593814b6
ahmet kizilay <ahmet.kizilay@gmail.com>
2015-05-18 01:57:09 -0400

    Removed changed_properties check in skip_conversion method
    changed_properties[attr] seems to be not nil even though the
    value is nil. Removed this check so that nil value is not sent to
    DateTimeConverter.to_db method.
    see #775

--- lib/neo4j/shared/type_converters.rb
+++ lib/neo4j/shared/type_converters.rb
@@ -133,9 +133,9 @@ module Neo4j::Shared
       end
     end
 
-    # Returns true if the property isn't defined in the model or it's both nil and unchanged.
+    # Returns true if the property isn't defined in the model or if it is nil
     def skip_conversion?(obj, attr, value)
-      !obj.class.attributes[attr] || (value.nil? && !obj.changed_attributes[attr])
+      !obj.class.attributes[attr] || value.nil?
     end
 
     class << self
