539faa91643f420ca140af43f2bf4784c05b83b3
Brian Underwood <ml+github@semi-sentient.com>
2015-06-28 16:50:12 -0600

    I guess probably nobody's used this before...

--- lib/neo4j/active_node/query/query_proxy_find_in_batches.rb
+++ lib/neo4j/active_node/query/query_proxy_find_in_batches.rb
@@ -4,13 +4,13 @@ module Neo4j
       module QueryProxyFindInBatches
         def find_in_batches(options = {})
           query.return(identity).find_in_batches(identity, @model.primary_key, options) do |batch|
-            yield batch
+            yield batch.map(&:identity)
           end
         end
 
         def find_each(options = {})
           query.return(identity).find_each(identity, @model.primary_key, options) do |result|
-            yield result
+            yield result.send(identity)
           end
         end
       end
