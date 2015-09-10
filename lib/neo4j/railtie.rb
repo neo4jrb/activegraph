ac188bd7ca92e227ca44f73f74efd4efdafd33ec
Brian Underwood <ml+github@semi-sentient.com>
2015-09-11 11:46:09 +1200

    Use debug log level for query logging

--- lib/neo4j/railtie.rb
+++ lib/neo4j/railtie.rb
@@ -79,7 +79,7 @@ module Neo4j
       Neo4j::Core::Query.pretty_cypher = Neo4j::Config[:pretty_logged_cypher_queries]
 
       Neo4j::Server::CypherSession.log_with do |message|
-        (Neo4j::Config[:logger] || Rails.logger).info message
+        (Neo4j::Config[:logger] || Rails.logger).debug message
       end
 
       @neo4j_cypher_logging_registered = true
