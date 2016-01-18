f9abda8c8babe5989c32588ea3fd3706aa0bdfee
djvs <djvansch@gmail.com>
2016-01-18 14:01:49 -0500

    update

--- lib/neo4j/shared/query_factory.rb
+++ lib/neo4j/shared/query_factory.rb
@@ -11,14 +11,13 @@ module Neo4j::Shared
     end
 
     def self.create(graph_object, identifier)
-      factory = case graph_object
-                when Neo4j::ActiveNode
-                  NodeQueryFactory
-                when Neo4j::ActiveRel
-                  RelQueryFactory
-                else
-                  fail "Unable to find factory for #{graph_object}"
-                end
+      if graph_object.respond_to?("node")
+        factory = NodeQueryFactory
+      elsif graph_object.respond_to?("rel_type")
+        factory = RelQueryFactory
+      else
+        fail "Unable to find factory for #{graph_object}"
+      end
       factory.new(graph_object, identifier)
     end
 
