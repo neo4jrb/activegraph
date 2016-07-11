4c5ff542b52da231d43d3116d51444f63c1264fe
ProGM <progiemmeh@gmail.com>
2016-07-11 10:57:35 +0200

    Fix minor code style issue.

--- spec/unit/active_node/node_list_formatter_spec.rb
+++ spec/unit/active_node/node_list_formatter_spec.rb
@@ -5,13 +5,13 @@ module Neo4j::ActiveNode
     subject { described_class.new(list, max_elements) }
 
     context 'when the list length is greater than `max_elements`' do
-      let(:list) { Array.new(10) { |i| i } }
+      let(:list) { (0...10).to_a }
 
       its(:inspect) { is_expected.to eq '[0, 1, 2, 3, 4, ...]' }
     end
 
     context 'when the list length is less or equal than `max_elements`' do
-      let(:list) { Array.new(5) { |i| i } }
+      let(:list) { (0...5).to_a }
 
       its(:inspect) { is_expected.to eq '[0, 1, 2, 3, 4]' }
     end
