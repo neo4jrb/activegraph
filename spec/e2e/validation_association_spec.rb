9e4dc72a91b48a96a0a77911e00894c56fa79455
Heinrich Klobuczek <heinrich@mail.com>
2016-08-31 08:10:33 +1200

    added different specs for assoc= and update

--- spec/e2e/validation_association_spec.rb
+++ spec/e2e/validation_association_spec.rb
@@ -16,8 +16,22 @@ describe Neo4j::ActiveNode::Validations do
 
     it 'should be valid with comments' do
       post = Post.new(comments: [Comment.create])
-      post.valid?
       expect(Post.new(comments: [Comment.create])).to be_valid
+    end
+
+    it 'direct comments assignment should ignore validation' do
+      post = Post.new(comments: [Comment.create])
+      post.comments = []
+      expect(post.reload.comments).to be_empty
+    end
+
+    it 'should not save on update_attributes if invalid' do
+      post = Post.create(comments: [Comment.create])
+      expect(post.update(comment_ids: [])).to be false
+      post.comments
+      # expect(Post.find(post.id).comments).not_to be_empty
+      # expect(post.reload.comments).not_to be_empty
+      expect(post.reload.comments).to be_present
     end
   end
 end
