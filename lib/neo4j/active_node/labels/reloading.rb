fbc02014c755a82c162225e10149d41d539159fe
Brian Underwood <ml+github@semi-sentient.com>
2016-09-14 11:32:25 -0400

    Copy change from yet_another_attempt_to_fix_reloading branch

--- lib/neo4j/active_node/labels/reloading.rb
+++ lib/neo4j/active_node/labels/reloading.rb
@@ -5,17 +5,8 @@ module Neo4j::ActiveNode::Labels
     MODELS_TO_RELOAD = []
 
     def self.reload_models!
-      MODELS_TO_RELOAD.each(&:constantize)
-      MODELS_TO_RELOAD.clear
-    end
-
-    module ClassMethods
-      def before_remove_const
-        associations.each_value(&:queue_model_refresh!)
-        MODELS_FOR_LABELS_CACHE.clear
-        WRAPPED_CLASSES.each { |c| MODELS_TO_RELOAD << c.name }
-        WRAPPED_CLASSES.clear
-      end
+      Neo4j::ActiveNode::Labels::WRAPPED_CLASSES.clear
+      Neo4j::ActiveNode::Labels.clear_wrapped_models
     end
   end
 end
