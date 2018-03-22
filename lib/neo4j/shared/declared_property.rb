30b36b45609e0f548de6ecaf2fc7589155f0f83b
Jamie Gaskins <jgaskins@gmail.com>
2018-03-21 23:34:16 -0400

    Optimize DeclaredProperty assignment
    Assigning to instance variables instead of pulling from a hash at
    time of assignment increases performance since it doesn't have to
    calculate a hash bucket each time.

--- lib/neo4j/shared/declared_property.rb
+++ lib/neo4j/shared/declared_property.rb
@@ -7,13 +7,17 @@ module Neo4j::Shared
     include Neo4j::Shared::DeclaredProperty::Index
 
     ILLEGAL_PROPS = %w(from_node to_node start_node end_node)
-    attr_reader :name, :name_string, :name_sym, :options, :magic_typecaster
+    attr_reader :name, :name_string, :name_sym, :options, :magic_typecaster, :type, :typecaster, :default_value
+    alias default default_value
 
-    def initialize(name, options = {})
+    def initialize(name, type: nil, typecaster: nil, default_value: nil, default: default_value, **options)
       fail IllegalPropertyError, "#{name} is an illegal property" if ILLEGAL_PROPS.include?(name.to_s)
       fail TypeError, "can't convert #{name.class} into Symbol" unless name.respond_to?(:to_sym)
       @name = @name_sym = name.to_sym
       @name_string = name.to_s
+      @type = type
+      @typecaster = typecaster
+      @default_value = default
       @options = options
       fail_invalid_options!
     end
@@ -54,19 +58,6 @@ module Neo4j::Shared
     def register
       register_magic_properties
     end
-
-    def type
-      options[:type]
-    end
-
-    def typecaster
-      options[:typecaster]
-    end
-
-    def default_value
-      options[:default]
-    end
-    alias default default_value
 
     def fail_invalid_options!
       case
