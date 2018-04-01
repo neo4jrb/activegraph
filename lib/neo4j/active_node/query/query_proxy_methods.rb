04013c5e043ca83f601e7e9ee33d87ecdea1ae33
Brian Underwood <brian@brian-underwood.codes>
2018-04-01 13:10:41 -0400

    Can define this variable inside of the else for a bit less computation

--- lib/neo4j/active_node/query/query_proxy_methods.rb
+++ lib/neo4j/active_node/query/query_proxy_methods.rb
@@ -255,11 +255,11 @@ module Neo4j
         end
 
         def first_and_last(func, target)
-          ord_prop = (func == LAST ? {order_property => :DESC} : order_property)
           new_query, pluck_proc = if self.query.clause?(:order)
                                     [self.query.with(identity),
                                      proc { |var| "#{func}(COLLECT(#{var})) as #{var}" }]
                                   else
+                                    ord_prop = (func == LAST ? {order_property => :DESC} : order_property)
                                     [self.order(ord_prop).limit(1),
                                      proc { |var| var }]
                                   end
