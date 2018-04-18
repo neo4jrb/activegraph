5ccee7110bebea68d7f9b61251f7d63f30a5489c
Brian Underwood <brian@brian-underwood.codes>
2018-04-17 21:09:23 -0400

    Use clear_changes_information because it's the official method

--- lib/neo4j/active_node/initialize.rb
+++ lib/neo4j/active_node/initialize.rb
@@ -10,7 +10,7 @@ module Neo4j::ActiveNode::Initialize
   def init_on_load(persisted_node, properties)
     self.class.extract_association_attributes!(properties)
     @_persisted_obj = persisted_node
-    changed_attributes_clear!
+    clear_changes_information
     @attributes = convert_and_assign_attributes(properties)
   end
 
