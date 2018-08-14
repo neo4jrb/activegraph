9d279d15415724f47351441765be314cf95de7a3
amitsuryavanshi <amitbsuryavanshi@gmail.com>
2018-08-14 21:15:26 +0530

    Spec refactor

--- spec/unit/active_node/scope_eval_context_spec.rb
+++ spec/unit/active_node/scope_eval_context_spec.rb
@@ -1,19 +1,18 @@
 describe Neo4j::ActiveNode::Scope::ScopeEvalContext do
   describe 'method missing' do
-    let(:target) { double('target') }
     let(:query_proxy) { Book.all }
     before(:example) do
       stub_active_node_class('Book')
     end
-    subject { described_class.new(target, query_proxy) }
+    subject { described_class.new(nil, query_proxy) }
 
     it 'should delegate non existant method call to query_proxy' do
-      expect(subject).to receive(:query)
+      expect(query_proxy).to receive(:query)
       subject.query
     end
 
     it 'should call method_missing of query_proxy in case method deos not exist on query_proxy' do
-      expect(query_proxy).to receive(:method_missing)
+      expect(query_proxy).to receive(:method_missing).with(:non_existent_method)
       subject.non_existent_method
     end
   end
