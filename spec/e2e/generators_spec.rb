9b2c160c19d344a30b9563380ba04ae3b5f3bbe0
Hardik Joshi <hardikjoshi1991@gmail.com>
2023-12-29 00:34:08 +0530

    corrected generators spec

--- spec/e2e/generators_spec.rb
+++ spec/e2e/generators_spec.rb
@@ -1,3 +1,7 @@
+require 'rails/generators/active_graph/model/model_generator'
+require 'rails/generators/active_graph/migration/migration_generator'
+require 'rails/generators/active_graph/upgrade_v8/upgrade_v8_generator'
+
 describe 'Generators' do
   around do |example|
     Timecop.freeze(Time.parse('1990-12-10 00:00:00 -0000')) { example.run }
@@ -5,7 +9,7 @@ describe 'Generators' do
 
   describe ActiveGraph::Generators::ModelGenerator do
     it 'has a `source_root`' do
-      expect(described_class.source_root).to include('active_graph/generators/model/templates')
+      expect(described_class.source_root).to include('rails/generators/active_graph/model/templates')
     end
 
     it 'creates a model and a migration file' do
@@ -17,7 +21,7 @@ describe 'Generators' do
 
   describe ActiveGraph::Generators::MigrationGenerator do
     it 'has a `source_root`' do
-      expect(described_class.source_root).to include('active_graph/generators/migration/templates')
+      expect(described_class.source_root).to include('rails/generators/active_graph/migration/templates')
     end
 
     it 'creates a migration file' do
@@ -38,7 +42,7 @@ describe 'Generators' do
     end
 
     it 'has a `source_root`' do
-      expect(described_class.source_root).to include('active_graph/generators/upgrade_v8/templates')
+      expect(described_class.source_root).to include('rails/generators/active_graph/upgrade_v8/templates')
     end
 
     it 'creates a migration file' do
