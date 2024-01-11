c4cac612146a708171ecc26170b388bbaca39a76
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-01-11 23:48:19 +0530

    use dynamic SHA

--- e2e_tests/setup.sh
+++ e2e_tests/setup.sh
@@ -3,7 +3,7 @@
 echo "SHA=$(git rev-parse --short "$GITHUB_SHA")" >> $GITHUB_ENV
 gem install rails -v 7.1.2 --no-document
 # /Users/hardik_joshi/work/open_source/activegraph/docs/activegraph.rb
-sed 's/.*activegraph.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "95600cacb82e1239ca1d105886630603b67a3280"/' docs/activegraph.rb > template.tmp
+sed 's/.*activegraph.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: '"$SHA"'/' docs/activegraph.rb > template.tmp
 rails _7.1.2_ new myapp -O -m ./template.tmp
 cd myapp
 bundle exec rails generate model User name:string
