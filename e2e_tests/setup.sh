cb0997840724c6f60db93394c0361fc08c78ef63
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-01-11 23:28:07 +0530

    corrected activegraph ref in script

--- e2e_tests/setup.sh
+++ e2e_tests/setup.sh
@@ -5,12 +5,11 @@ git_hash=$(git rev-parse --short "$GITHUB_SHA")
 echo "SHA=$(git_hash)" >> $GITHUB_ENV
 gem install rails -v 7.1.2 --no-document
 # /Users/hardik_joshi/work/open_source/activegraph/docs/activegraph.rb
-sed 's/.*activegraph.*/gem "activegraph", github: "neo4jrb\/activegraph", ref: "95600cacb82e1239ca1d105886630603b67a3280"/' docs/activegraph.rb > template.tmp
+sed 's/.*activegraph.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "95600cacb82e1239ca1d105886630603b67a3280"/' docs/activegraph.rb > template.tmp
 rails _7.1.2_ new myapp -O -m ./template.tmp
-# sed 's/.*activegraph.*/gem "activegraph", path: "..\/"/' Gemfile > Gemfile.bak && mv Gemfile.bak Gemfile
 cd myapp
 bundle exec rails generate model User name:string
-RAILS_ENV=test bundle exec rake neo4j:migrate
+# RAILS_ENV=test bundle exec rake neo4j:migrate
 # bundle exec rails c
 # bundle exec rails s
 
