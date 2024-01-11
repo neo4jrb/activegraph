08874af33ea1cec666e153e5deeaacfc03ff0a01
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-01-12 00:03:39 +0530

    debug action

--- e2e_tests/setup.sh
+++ e2e_tests/setup.sh
@@ -4,6 +4,7 @@ echo "SHA=$(git rev-parse --short "$GITHUB_SHA")" >> $GITHUB_ENV
 gem install rails -v 7.1.2 --no-document
 # /Users/hardik_joshi/work/open_source/activegraph/docs/activegraph.rb
 sed 's/.*activegraph.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "'"$SHA"'"/' docs/activegraph.rb > template.tmp
+echo "template.tmp=$(cat template.tmp)" >> $GITHUB_OUTPUT
 rails _7.1.2_ new myapp -O -m ./template.tmp
 cd myapp
 bundle exec rails generate model User name:string
