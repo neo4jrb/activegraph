81942b3f12b766e6b56335a8657418cf37e98642
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-01-11 22:34:25 +0530

    e2e tests github action

---
+++ e2e_tests/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# git_hash=$(git rev-parse --short "$GITHUB_SHA")
+# git_branch=${GITHUB_REF#refs/heads/}
+echo "SHA=$(git rev-parse --short "$GITHUB_SHA")" >> $GITHUB_ENV
+gem install rails -v 7.1.2 --no-document
+# /Users/hardik_joshi/work/open_source/activegraph/docs/activegraph.rb
+sed 's/.*activegraph.*/gem "activegraph", github: "neo4jrb\/activegraph", ref: "95600cacb82e1239ca1d105886630603b67a3280"/' docs/activegraph.rb > template.tmp
+rails _7.1.2_ new myapp -O -m ./template.tmp
+# sed 's/.*activegraph.*/gem "activegraph", path: "..\/"/' Gemfile > Gemfile.bak && mv Gemfile.bak Gemfile
+cd myapp
+bundle exec rails generate model User name:string
+RAILS_ENV=test bundle exec rake neo4j:migrate
+# bundle exec rails c
+# bundle exec rails s
+
