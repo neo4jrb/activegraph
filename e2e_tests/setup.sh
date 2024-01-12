d56897397b3a6ea1b7300ae7a65f1846beb214c7
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-01-12 23:34:58 +0530

    Update setup.sh

--- e2e_tests/setup.sh
+++ e2e_tests/setup.sh
@@ -9,8 +9,25 @@ else
   sed 's/.*gem '"'"'activegraph'"'"'.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "'"$(git rev-parse "$GITHUB_SHA")"'"/' docs/activegraph.rb > template.tmp
 fi
 rails _7.1.2_ new myapp -O -m ./template.tmp
+rm -f ./template.tmp
 cd myapp
+
+if [[ -n "$E2E_PORT" ]]
+then
+  sed 's/7687/'$E2E_PORT'/' config/environments/development.rb > dev_env.tmp
+  mv dev_env.tmp config/environments/development.rb
+fi
+
+if [[ -n "$E2E_NO_CRED" ]]
+then
+  sed "s/'neo4j'/''/" config/environments/development.rb > dev_env.tmp
+  mv dev_env.tmp config/environments/development.rb
+  sed "s/'password'/''/" config/environments/development.rb > dev_env.tmp
+  mv dev_env.tmp config/environments/development.rb
+fi
+
 bundle exec rails generate model User name:string
+bundle exec rails generate migration BlahMigration
 bundle exec rake neo4j:migrate
 if echo 'puts "hi"' | bundle exec rails c
 then
@@ -21,5 +38,3 @@ fi
 bundle exec rails s -d
 while [ $((curl localhost:3000/ > /dev/null 2>&1); echo $?) -ne 0 ]; do sleep 1; done
 kill `cat tmp/pids/server.pid`
-
-
