8e5402566ccf6e9fbe39ef1c351133aa5cb5e941
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-01-12 21:22:35 +0530

    corrected script to run in dev environment

--- e2e_tests/setup.sh
+++ e2e_tests/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-echo "SHA=$(git rev-parse "$GITHUB_SHA")" >> $GITHUB_ENV
 gem install rails -v 7.1.2 --no-document
-sed 's/.*gem '"'"'activegraph'"'"'.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "'"$(git rev-parse "$GITHUB_SHA")"'"/' docs/activegraph.rb > template.tmp
+if [[ -n "$SHA" ]]
+then
+  sed 's/.*gem '"'"'activegraph'"'"'.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "'"$SHA"'"/' docs/activegraph.rb > template.tmp
+else
+  echo "SHA=$(git rev-parse "$GITHUB_SHA")" >> $GITHUB_ENV
+  sed 's/.*gem '"'"'activegraph'"'"'.*/gem '"'"'activegraph'"'"', github: "neo4jrb\/activegraph", ref: "'"$(git rev-parse "$GITHUB_SHA")"'"/' docs/activegraph.rb > template.tmp
+fi
 rails _7.1.2_ new myapp -O -m ./template.tmp
 cd myapp
 bundle exec rails generate model User name:string
-RAILS_ENV=test bundle exec rake neo4j:migrate
+bundle exec rake neo4j:migrate
 # bundle exec rails c
 # bundle exec rails s
+
 
