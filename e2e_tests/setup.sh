7d0863efb044c36b9fa99639a409a014105602d2
Hardik Joshi <hardikjoshi1991@gmail.com>
2024-09-24 16:08:32 +0530

    improved e2e tests

--- e2e_tests/setup.sh
+++ e2e_tests/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Example values of environment variables ACTIVE_MODEL_VERSION=7.1.3 ACTIVEGRAPH_PATH=../ E2E_PORT=7687 E2E_NO_CRED=true
 gem install rails -v $ACTIVE_MODEL_VERSION --no-document
 
 if [[ -n "$ACTIVEGRAPH_PATH" ]]
@@ -28,18 +29,23 @@ then
   mv dev_env.tmp config/environments/development.rb
 fi
 
+echo "Generating model"
 bundle exec rails generate model User name:string
+echo "Generating migration"
 bundle exec rails generate migration BlahMigration
+echo "Running migration"
 bundle exec rake neo4j:migrate
 
-if echo 'puts "hi"' | bundle exec rails c
+if echo 'puts "Starting rails console"' | bundle exec rails c
 then
-  echo "rails console works correctly"
+  echo "Rails console works correctly"
 else
+  echo "Rails console didn't start"
   exit 1
 fi
 
-bundle exec rails s -d
+echo "Starting rails server"
+bundle exec rails s &
 until $(curl --output /dev/null --silent --head --fail localhost:3000); do
   printf '.'
   sleep 1
